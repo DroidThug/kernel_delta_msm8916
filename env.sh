424972b539f2c7f6276adf4c4d9f9100bf079548
Sai Vishal <saivishal2001@gmail.com>
2016-09-02 14:00:32 +0000

    And its Delta!
    Signed-off-by: Sai Vishal <saivishal2001@gmail.com>

---
+++ env.sh
@@ -0,0 +1,13 @@
+KERNEL_DIR=/and/thug/hydra
+KERN_IMG=$KERNEL_DIR/arch/arm64/boot/Image
+DTBTOOL=$KERNEL_DIR//tools/dtbToolCM
+TOOLCHAIN_DIR=~/and/tc/ub/bin
+export ARCH=arm64
+# export CCOMPILE=$CROSS_COMPILE
+# export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE=/and/thug/tc/ub/bin/aarch64-linux-android-
+export PATH=$PATH:~/and/thug/tc/ub/bin
+export KBUILD_BUILD_USER="Thug"
+export KBUILD_BUILD_HOST="EvoqueUnit"
+STRIP="/and/thug/tc/ub/bin/aarch64-linux-android-strip"
+MODULES_DIR=$KERNEL_DIR/drivers/staging/prima
